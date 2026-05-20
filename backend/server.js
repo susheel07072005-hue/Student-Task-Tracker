9a7002ea0dd6f3494218bdc1d6e25bdc5eb74179
susheel07072005 <susheel07072005@gmail.com>
2026-05-21 02:33:46 +0530

    Fixed Render backend

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.post("/tasks", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
